c00e611273e27df888aecacf9c87e49033ad14f0
PEREZ MANSILLA Diego <diego.perez2@soprasteria.com>
2021-01-15 14:20:52 +0100

    Testing ended

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -4,9 +4,13 @@ import { shallow } from 'enzyme';
 import CounterApp from '../CounterApp';
 
 describe('Pruebas de  <CounterApp/>', () => {
-	test('Debe mostrar <CounterApp/> correctamente', () => {
-		const wrapper = shallow(<CounterApp />);
+	let wrapper = shallow(<CounterApp />);
 
+	beforeEach(() => {
+		wrapper = shallow(<CounterApp />);
+	});
+
+	test('Debe mostrar <CounterApp/> correctamente', () => {
 		expect(wrapper).toMatchSnapshot();
 	});
 
@@ -16,5 +20,29 @@ describe('Pruebas de  <CounterApp/>', () => {
 		const counter = wrapper.find('h2').text().trim();
 
 		expect(counter).toBe('100');
+	});
+
+	test('Debe de incrementar con el botón +1', () => {
+		wrapper.find('button').at(0).simulate('click');
+		const counter = wrapper.find('h2').text().trim();
+
+		expect(counter).toBe('11');
+	});
+
+	test('Debe de decrementar con el botón -1', () => {
+		wrapper.find('button').at(2).simulate('click');
+		const counter = wrapper.find('h2').text().trim();
+
+		expect(counter).toBe('9');
+	});
+
+	test('Debe de colocar el valor por defecto con el botón reset', () => {
+		const wrapper = shallow(<CounterApp value={105} />);
+
+		wrapper.find('button').at(0).simulate('click');
+		wrapper.find('button').at(1).simulate('click');
+		const counter = wrapper.find('h2').text().trim();
+
+		expect(counter).toBe('105');
 	});
 });
