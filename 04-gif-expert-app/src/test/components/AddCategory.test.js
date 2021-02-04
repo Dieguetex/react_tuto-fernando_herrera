a2913ba09195d43510e08adae6205500a5ab9118
PEREZ MANSILLA Diego <diego.perez2@soprasteria.com>
2021-02-04 15:07:54 +0100

    Added new test

--- 04-gif-expert-app/src/test/components/AddCategory.test.js
+++ 04-gif-expert-app/src/test/components/AddCategory.test.js
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme';
 import { AddCategory } from '../../components/AddCategory';
 
 describe('Pruebas en <AddCategoy />', () => {
-	const setCategories = () => {};
-	const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+	const setCategories = jest.fn();
+	let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		wrapper = shallow(<AddCategory setCategories={setCategories} />);
+	});
 
 	test('Debe mostrarse correctamente', () => {
 		expect(wrapper).toMatchSnapshot();
@@ -17,5 +22,23 @@ describe('Pruebas en <AddCategoy />', () => {
 		input.simulate('change', { target: { value } });
 
 		expect(wrapper.find('p').text().trim()).toBe(value);
+	});
+
+	test('No debe de postear la informacion con submit', () => {
+		wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+		expect(setCategories).not.toHaveBeenCalled();
+	});
+
+	test('Debe de llamar el setCategories y limpiar la caja de texto', () => {
+		const input = wrapper.find('input');
+		const value = 'hola mundo';
+
+		input.simulate('change', { target: { value } });
+		wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+		expect(setCategories).toHaveBeenCalledTimes(1);
+		expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+		expect(input.prop('value')).toBe('');
 	});
 });
